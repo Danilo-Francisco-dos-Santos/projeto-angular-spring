626f0b4eb3a390091d6374afbfa127f1737f189a
Danilo Santos <danilohhe@gmail.com>
2024-01-16 00:23:38 -0300

    criado tabela cursos

--- src/app/cursos/cursos.module.ts
+++ src/app/cursos/cursos.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CursosRoutingModule } from './cursos-routing.module';
-
+import {MatTableModule} from '@angular/material/table';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    CursosRoutingModule
+    CursosRoutingModule,
+    MatTableModule
   ]
 })
 export class CursosModule { }
--- src/app/cursos/cursos/cursos.component.ts
+++ src/app/cursos/cursos/cursos.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatTableModule } from '@angular/material/table';
+import { Curso } from '../model/curso';
 
 @Component({
   selector: 'app-cursos',
   standalone: true,
-  imports: [],
+  imports: [MatTableModule],
   templateUrl: './cursos.component.html',
   styleUrl: './cursos.component.css'
 })
-export class CursosComponent {
+export class CursosComponent implements OnInit {
+
+  cursos: Curso[] = [
+    { _id: '1', nome: 'Angular', categoria: 'Front-End' }
+  ];
+
+  displayedColumns = ['nome', 'categoria'];
+
+  constructor() {
+    //this.cursos = [];
+  }
+
+  ngOnInit(): void {
+
+  }
 
 }
